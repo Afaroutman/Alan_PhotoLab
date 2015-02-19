4e89d2d3a42f8a56d8716ae52dfe45de36e21df9
Alan <alan.hite24@gmail.com>
2015-02-19 09:10:25 -0700

    PhotoShop base Repo

---
+++ src/pix/view/FauxToeShopPanel.java
@@ -0,0 +1,262 @@
+package pix.view;
+
+import java.awt.Color;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SpringLayout;
+
+import pix.controller.FauxToeShopController;
+import pixLab.classes.ImageDisplay;
+import pixLab.classes.Picture;
+
+
+	public class FauxToeShopPanel extends JPanel
+	{
+
+		private FauxToeShopController baseController;	
+			
+		private JComboBox<String> pictureBox;
+		private JComboBox<String> filterBox;
+		private JScrollPane imagePane;
+		private SpringLayout baseLayout;
+		private Picture basePicture;
+		private Picture selectedPicture;
+		
+		public FauxToeShopPanel(FauxToeShopController baseController)
+		{
+			this.baseController = baseController;
+			
+			
+			imagePane = new JScrollPane();
+			basePicture = new Picture("zinogre3.jpg");
+			selectedPicture = new Picture("zinogre3.jpg");
+			baseLayout = new SpringLayout();
+			
+			setupPicture();
+			setupComboBox();
+			setupPanel();
+			setupLayout();
+			setupListeners();
+		}
+		
+		private void setupPicture()
+		{
+			BufferedImage bufferedPic = basePicture.getBufferedImage();
+			ImageDisplay picDisplay = new ImageDisplay(bufferedPic);
+			imagePane.setViewportView(picDisplay);
+		}
+		
+		private void resetButton()
+		{
+			basePicture = new Picture(selectedPicture.getFileName());
+		}
+		private void setupComboBox()
+		{
+			String [] filterArray = 
+			{	
+			"Reset", //0
+			"KeepOnlyBlue", //1
+			"KeepOnlyRed", //2
+			"KeepOnlyGreen", //3
+			"Negate", //4 
+			"Grayscale", //5
+			"FixUnderwater", //6
+			"MirrorVertical", //7
+			"MirrorHorizontal", //8
+			"MirrorHorizontalBottomToTop", //9
+			"MirrorDiagonal", //10
+			"MirrorTemple", //11
+			"MirrorArms", //12
+			"MirrorGull", //13
+			"ColorFilter", //14
+			"MakeFilter", //15
+			"Copy",  //16
+			"EdgeDetection", //17
+			};
+			filterBox = new JComboBox (filterArray);
+	
+			
+				String [] pictureArray = 
+				{
+					"Default",
+					"Temple",
+					"seagull",
+					"snowman",
+				};
+			pictureBox = new JComboBox (pictureArray);
+			
+			}
+			
+		
+		private void setupPanel()
+		{
+			this.setBackground(Color.GREEN);
+			this.setLayout(baseLayout);
+			this.add(filterBox);
+			this.add(imagePane);
+			this.add(pictureBox);
+		}
+		private void setupLayout()
+		{
+			baseLayout.putConstraint(SpringLayout.NORTH, imagePane, 100, SpringLayout.NORTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, imagePane, 50, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.NORTH, filterBox, 25, SpringLayout.NORTH, this);
+			baseLayout.putConstraint(SpringLayout.WEST, filterBox, 211, SpringLayout.WEST, this);
+			baseLayout.putConstraint(SpringLayout.NORTH, pictureBox, 0, SpringLayout.NORTH, filterBox);
+			baseLayout.putConstraint(SpringLayout.WEST, pictureBox, 0, SpringLayout.WEST, imagePane);
+		}
+		private void setupListeners()
+		{
+			filterBox.addItemListener(new ItemListener()
+			{
+				public void itemStateChanged(ItemEvent currentEvent)
+				{
+					if(filterBox.getSelectedIndex() == 0)
+					{
+						resetButton();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 1)
+					{
+						resetButton();
+						basePicture.testKeepOnlyBlue();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 2)
+					{
+						resetButton();
+						basePicture.testKeepOnlyRed();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 3)
+					{
+						resetButton();
+						basePicture.testKeepOnlyGreen();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 4)
+					{
+						resetButton();
+						basePicture.Negate();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 5)
+					{
+						resetButton();
+						basePicture.Grayscale();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 6)
+					{
+						resetButton();
+						basePicture.FixUnderwater();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 7)
+					{
+						resetButton();
+						basePicture.mirrorVertical();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 8)
+					{
+						resetButton();
+						//basePicture.MirrorHorizontal();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 9)
+					{
+						resetButton();
+						basePicture.mirrorHorizontalBottomToTop();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 10)
+					{
+						resetButton();
+						basePicture.mirrorDiagonal();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 11)
+					{
+						resetButton();
+						basePicture.mirrorTemple();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 12)
+					{
+						resetButton();
+						basePicture.MirrorArms();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 13)
+					{
+						resetButton();
+						basePicture.MirrorGull();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 14)
+					{
+						resetButton();
+						basePicture.rotateColorFilter();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 15)
+					{
+						resetButton();
+						basePicture.makeFilter();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 16)
+					{
+						resetButton();
+						//basePicture.Copy();
+						setupPicture();
+					}
+					else if(filterBox.getSelectedIndex() == 17)
+					{
+						resetButton();
+						basePicture.edgeDetection(50);
+						setupPicture();
+					}
+				}
+			});
+			pictureBox.addItemListener(new ItemListener()
+			{
+				public void itemStateChanged(ItemEvent currentEvent)
+				{
+					if(pictureBox.getSelectedIndex() == 0)
+					{
+						basePicture = new Picture("zinogre3.jpg");
+						selectedPicture = new Picture("zinogre3.jpg");
+						setupPicture();
+					}
+					else if(pictureBox.getSelectedIndex() == 1)
+					{
+						basePicture = new Picture("Temple.jpg");
+						selectedPicture = new Picture("Temple.jpg");
+						setupPicture();
+					}
+					else if(pictureBox.getSelectedIndex() == 2)
+					{
+						basePicture = new Picture("seagull.jpg");
+						selectedPicture = new Picture("seagull.jpg");
+						setupPicture();
+					}
+					else if(pictureBox.getSelectedIndex() == 3)
+					{
+						basePicture = new Picture("snowman.jpg");
+						selectedPicture = new Picture("snowman.jpg");
+						setupPicture();
+					}
+					
+				}
+			}
+					);
+		}
+	}
+
---
+++ src/pixLab/classes/PictureTester.java
@@ -0,0 +1,165 @@
+package pixLab.classes;
+/**
+ * This class contains class (static) methods
+ * that will help you test the Picture class 
+ * methods.  Uncomment the methods and the code
+ * in the main to test.
+ * 
+ * @author Barbara Ericson 
+ */
+public class PictureTester
+{
+  /** Method to test zeroBlue */
+  public static void testZeroBlue()
+  {
+    Picture beach = new Picture("beach.jpg");
+    beach.explore();
+    beach.zeroBlue();
+    beach.explore();
+  }
+  public static void testMirrorHorizontal()
+  {
+	  Picture zinogre = new Picture("zinogre3.jpg");
+	  zinogre.explore();
+	//  zinogre.MirrorHorizontal();
+	  zinogre.explore();
+  }
+  
+  /** Method to test mirrorVertical */
+  public static void testMirrorVertical()
+  {
+    Picture caterpillar = new Picture("zinogre3.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVertical();
+    caterpillar.explore();
+  }
+  
+  /** Method to test mirrorTemple */
+  public static void testMirrorTemple()
+  {
+    Picture temple = new Picture("temple.jpg");
+    temple.explore();
+    temple.mirrorTemple();
+    temple.explore();
+  }
+  
+  /** Method to test the collage method */
+  public static void testCollage()
+  {
+    Picture canvas = new Picture("640x480.jpg");
+    canvas.createCollage();
+    canvas.explore();
+  }
+  
+  /** Method to test edgeDetection */
+  public static void testEdgeDetection()
+  {
+    Picture swan = new Picture("swan.jpg");
+    swan.explore();
+    swan.edgeDetection(10);
+   
+  }
+  public static void testKeepOnlyBlue()
+  {
+	  Picture blueMark = new Picture("blue-mark.jpg");
+	  blueMark.explore();
+	  blueMark.testKeepOnlyBlue();
+  }
+  public static void testKeepOnlyRed()
+  {
+	  Picture gorge = new Picture("george.jpg");
+	  gorge.explore();
+	  gorge.testKeepOnlyRed();
+  }
+  public static void testKeepOnlyGreen()
+  {
+	  Picture wall = new Picture("wall.jpg");
+	  wall.explore();
+	  wall.testKeepOnlyRed();
+  }
+  public static void testNegate()
+  {
+	  Picture illuminarty = new Picture ("illuminarty.jpg");
+	  illuminarty.explore();
+	  illuminarty.Negate();
+  }
+  public static void testGrayscale()
+  {
+	  Picture snowman = new Picture("snowman.jpg");
+	  snowman.explore();
+	  snowman.Grayscale();
+  }
+  public static void testFixUnderwater()
+  {
+	  Picture water = new Picture("water.jpg");
+	  water.explore();
+	  water.FixUnderwater();
+  }
+  public static void testMirrorArms()
+  {
+	  Picture snowman = new Picture("snowman.jpg");
+	  snowman.MirrorArms();
+	  snowman.explore();
+	  
+  }
+  public static void testMirrorGull()
+  {
+	  Picture seagull = new Picture("seagull.jpg");
+	  seagull.MirrorGull();
+	  seagull.explore();
+  }
+  public static void testMirrorDiagonal()
+  {
+	  Picture beach = new Picture("beach.jpg");
+	  beach.mirrorDiagonal();
+	  beach.explore();
+  }
+  public static void testMakeFilter()
+  {
+	  Picture caterpillar = new Picture("caterpillar.jpg");
+	  caterpillar.makeFilter();
+	  caterpillar.explore();
+  }
+  public static void testColorFilter()
+  {
+	  Picture zinogre = new Picture("zinogre.jpg");
+	  zinogre.explore();
+	  zinogre.rotateColorFilter();
+	  zinogre.explore();
+  }
+  /** Main method for testing.  Every class can have a main
+    * method in Java */
+  public static void main(String[] args)
+  {
+    // uncomment a call here to run a test
+    // and comment out the ones you don't want
+    // to run
+//	    testZeroBlue();
+//	    testKeepOnlyBlue();
+//	    testKeepOnlyRed();
+//	    testKeepOnlyGreen();
+//	    testNegate();
+//      testGrayscale();
+//	    testFixUnderwater();
+//	    testMirrorVertical();
+	    testMirrorHorizontal();
+//	    testMirrorTemple();
+//	    testMirrorArms();
+//	    testMirrorGull();
+//	    testMirrorDiagonal();
+//	  	testColorFilter();
+//	    testMakeFilter();
+//	    testCollage();
+//	    testCopy();
+//	    testEdgeDetection();
+//	    testEdgeDetection2();
+//	    testChromakey();
+//	    testEncodeAndDecode();
+//	    testGetCountRedOverValue(250);
+//	    testSetRedToHalfValueInTopHalf();
+//	    testClearBlueOverValue(200);
+//	    testGetAverageForColumn(0);
+  }
+
+ 
+}
